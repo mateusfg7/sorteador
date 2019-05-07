18169f918cd4e572f62e2b8711bb1486865bb929
Mateus Felipe <mateusfelipefg77@gmail.com>
2019-05-07 14:33:29 -0300

    v1.1 (add: numero de sorteios seguidos)

--- sorteio.py
+++ sorteio.py
@@ -1,12 +1,22 @@
-import random
+import random # biblioteca de ramdomisação de numeros
 
 # pega o valor mínimo
-minimo = int(input("Valor mínimo: "))
+minimo = int(input("Valor mínimo: \033[01;32m"))
 # pega o valor máximo
-maximo = int(input("Valor máximo: "))
+maximo = int(input("\033[00;00;00mValor máximo: \033[01;32m"))
+# pega o numero de sorteios
+numSort = int(input("\033[00;00;00mNumero de sorteios: \033[01;32m"))
 
-# calcula o numero aleatório
-num = random.randint(minimo, maximo)
+idSort = 1 # identificação do sorteio
 
-# mostra na tela
-print(num)
+print("\033[00;00;00m\n")
+
+# loop de sorteio
+for x in range(numSort):
+	# calcula o numero aleatório
+	num = random.randint(minimo, maximo)
+	# mostra na tela
+	print("\033[00;31mSorteio {}: \033[01;37;41m{}\033[00;00;00m".format(idSort, num))
+	idSort += 1
+
+print("\n")
